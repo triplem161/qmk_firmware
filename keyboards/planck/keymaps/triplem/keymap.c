dce8a381a78292704a9d9eb6d814b1e521938720
Marion Maignan <maigmarion@gmail.com>
2018-01-06 21:20:57 +0100

    My own keyboards config : first commit

---
+++ keyboards/planck/keymaps/triplem/keymap.c
@@ -0,0 +1,10 @@
+#include "planck.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = {
+    {KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A},
+    {KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A},
+    {KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A},
+    {KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A}
+  }
+};
