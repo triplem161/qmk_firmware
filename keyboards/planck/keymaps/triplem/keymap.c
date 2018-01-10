921eafc66f7187d511c2e668ca6ca92bfb131557
Triplem <maigmarion@gmail.com>
2018-01-10 22:26:10 +0100

    Test on changing key

--- keyboards/planck/keymaps/triplem/keymap.c
+++ keyboards/planck/keymaps/triplem/keymap.c
@@ -2,9 +2,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = {
-    {KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A},
-    {KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A},
-    {KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A},
-    {KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A, KC_A}
+    {KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B},
+    {KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B},
+    {KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B},
+    {KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B}
   }
 };
