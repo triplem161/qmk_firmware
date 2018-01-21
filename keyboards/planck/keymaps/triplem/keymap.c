ff780d21ffdb5730e9dd328c323776cb57e26477
Marion Maignan <maigmarion@gmail.com>
2018-01-21 20:58:02 +0100

    Start on own layout

--- keyboards/planck/keymaps/triplem/keymap.c
+++ keyboards/planck/keymaps/triplem/keymap.c
@@ -1,10 +1,15 @@
 #include "planck.h"
 
+enum LAYER {
+  DVORAK,
+  NUM
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = {
-    {KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B},
-    {KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B},
-    {KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B},
-    {KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B, KC_B}
+  [DVORAK] = {
+    {KC_TAB,   KC_SCOLON,  KC_COMM,  KC_DOT,  KC_P,   KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_BSPACE},
+    {KC_LCTRL, KC_A,     KC_O,     KC_E,    KC_U,   KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_ENTER},
+    {KC_LSFT,  KC_QUOT,  KC_Q,     KC_J,    KC_K,   KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,     _______},
+    {_______,  _______,  _______,  KC_LALT, KC_SPC, _______,  _______,  _______,  _______,  _______,  _______,  _______}
   }
 };
